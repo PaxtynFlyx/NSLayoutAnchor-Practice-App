b3e6bb400a53cc299b79018e6afae19748ed8bec
tetraprism95 <nchundev99@gmail.com>
2021-08-01 15:25:32 -0400

    Extend NSLayoutAnchor class to help facilitate constraints

--- Auto Layout Constraint App/ViewController.swift
+++ Auto Layout Constraint App/ViewController.swift
@@ -8,12 +8,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private let containerView: UIView = {
+        let view = UIView()
+        view.backgroundColor = .green
+        
+        return view
+    }()
 
+    private let containerView2: UIView = {
+        let view = UIView()
+        view.backgroundColor = .orange
+        
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+        configureUI()
     }
-
-
+    
+    private func configureUI() {
+        view.addSubview(containerView)
+        containerView.addSubview(containerView2)
+        
+        containerView.anchorDimensions(size: .init(width: 200, height: 200))
+        containerView.centerXY()
+        containerView2.anchorDimensions(size: .init(width: 50, height: 50))
+        containerView2.centerXY()
+    }
 }
 
